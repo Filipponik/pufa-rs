d2e08913443fe1ceb7cf214f331d9230f7d98fda
Filipponik <mitin.maks@bk.ru>
2025-02-08 16:25:28 +0300

    feat(server): Moved server logic to server module

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,10 @@
 mod pufa;
+mod server;
+mod config;
 
-use axum::{http::StatusCode, response::Json, routing::get, Router};
-use serde_json::{json, Value};
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(get_pufa_word));
-
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
-    axum::serve(listener, app).await.unwrap();
-}
-
-async fn get_pufa_word() -> (StatusCode, Json<Value>) {
-    let pufa_word = pufa::get_cached_result().await;
-    match pufa_word {
-        Err(error) => (
-            StatusCode::SERVICE_UNAVAILABLE,
-            Json(json!({
-                "success": false,
-                "data": null,
-                "error_message": error.to_string(),
-                "updated_at": null,
-            })),
-        ),
-        Ok(cache_state) => (
-            StatusCode::OK,
-            Json(json!({
-                "success": true,
-                "data": cache_state.last_word,
-                "error_message": null,
-                "updated_at": cache_state.get_formatted_updated_at(),
-            })),
-        ),
-    }
+    config::setup_tracing();
+    server::start_server().await;
 }
---
+++ src/server.rs
@@ -0,0 +1,78 @@
+use crate::pufa;
+use axum::http::StatusCode;
+use axum::routing::get;
+use axum::{Json, Router};
+use serde::Serialize;
+use tracing::info;
+
+pub async fn start_server() {
+    let app = Router::new().route("/", get(get_pufa_word));
+    let port: u16 = 3000;
+    let addr = format!("0.0.0.0:{}", port);
+
+    let listener = tokio::net::TcpListener::bind(&addr).await.unwrap();
+    info!("Server started at http://{addr}, connect from localhost at http://localhost:{port}");
+    axum::serve(listener, app).await.unwrap();
+}
+
+struct Response {
+    status: StatusCode,
+    body: ResponseBody,
+}
+
+impl Response {
+    fn new(status: StatusCode, body: ResponseBody) -> Self {
+        Self { status, body }
+    }
+
+    fn to_axum_response(&self) -> (StatusCode, Json<ResponseBody>) {
+        (self.status, Json(self.body.clone()))
+    }
+}
+
+#[derive(Clone, Debug, Serialize)]
+struct ResponseBody {
+    success: bool,
+    data: Option<String>,
+    error_message: Option<String>,
+    updated_at: Option<String>,
+}
+
+impl ResponseBody {
+    fn new_success(word: &str, updated_at: &str) -> Self {
+        Self {
+            success: true,
+            data: Some(word.to_string()),
+            error_message: None,
+            updated_at: Some(updated_at.to_string()),
+        }
+    }
+
+    fn new_error(error_message: &str) -> Self {
+        Self {
+            success: false,
+            data: None,
+            error_message: Some(error_message.to_string()),
+            updated_at: None,
+        }
+    }
+}
+
+async fn get_pufa_word() -> (StatusCode, Json<ResponseBody>) {
+    let pufa_word = pufa::get_cached_result().await;
+    let response = match pufa_word {
+        Err(error) => Response::new(
+            StatusCode::SERVICE_UNAVAILABLE,
+            ResponseBody::new_error(&error.to_string()),
+        ),
+        Ok(cache_state) => Response::new(
+            StatusCode::SERVICE_UNAVAILABLE,
+            ResponseBody::new_success(
+                &cache_state.last_word,
+                &cache_state.get_formatted_updated_at(),
+            ),
+        ),
+    };
+
+    response.to_axum_response()
+}
